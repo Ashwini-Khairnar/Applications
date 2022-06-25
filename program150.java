32f106162f1ca45609fbce62e337ee38b9d4364a
Ashwini-Khairnar <106311899+Ashwini-Khairnar@users.noreply.github.com>
2022-06-25 18:02:54 +0530

    Add files via upload

---
+++ program150.java
@@ -0,0 +1,36 @@
+// toggle the 4th and 7th bit
+import java.lang.*;
+import java.util.*;
+
+// 0000 0000 0000 0000 0000 0000 0100 1000
+//   0    0   0    0     0   0    4     8
+class Bitwise
+{
+    public int OnBit(int iNo)       //4
+    {
+        int iMask = 0X00000048;     
+        int iResult = 0;
+
+        iResult = iNo ^ iMask;  
+        return iResult; 
+    }
+    
+}
+
+class program150
+{
+    public static void main(String arg[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter number :");
+        int value = sobj.nextInt();
+        
+        Bitwise bobj = new Bitwise();
+        int iret = bobj.OnBit(value);
+
+        System.out.println("Updated number is : "+iret);
+
+        
+    }
+}
